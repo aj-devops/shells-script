5b6f650c5e5e2b143f58acb4484b89a106c6ea25
ajay <appuajay08@gmail.com>
2020-03-28 22:51:19 +0530

    print examples

--- print.sh
+++ print.sh
@@ -23,3 +23,33 @@ echo Hello\nHow are you
 
 # With out quotes \ will be ignored 
 
+echo "Hello\nHow are you"
+
+## Escape sequences in echo command will work only when you enable with -e option.
+
+echo -e "Hello\nHow are you\nBye"
+
+## Printing tab spaces
+echo -e "Name\t\tMarks\nRaju\t\t20\nRamu\t\t30"
+
+
+## We can print some text in color to grab the user attendtion.
+
+# Shell has total 6 colors 
+# RGYBMC, Red, Green, yellow, Blue, Magenta, Cyan
+
+# Color         # FG Color             # BG Color 
+# Red             31                      41
+# Green           32                      42  
+# Yellow          33                      43
+# Blue            34                      44
+# Magenta         35                      45
+# Cyan            36                      46
+
+# echo -e "\e[COLmHello\e[0m"
+# \e -> \033
+
+echo -e "\e[31mHello in red\e[0m"
+
+## Follow this for more colors.
+## https://misc.flogisoft.com/bash/tip_colors_and_formatting
